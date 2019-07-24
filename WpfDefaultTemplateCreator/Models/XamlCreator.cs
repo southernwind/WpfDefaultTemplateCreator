385713f972a09ed893b341fde6d40016634c2753
southernwind <testakina@gmail.com>
2019-07-24 13:23:59 +0900

    コントロール選択用のListBoxを追加

--- WpfDefaultTemplateCreator/Models/XamlCreator.cs
+++ WpfDefaultTemplateCreator/Models/XamlCreator.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Markup;
@@ -7,8 +8,10 @@ using System.Xml;
 namespace WpfDefaultTemplateCreator.Models {
 	internal static class XamlCreator {
 		public static string Create(Type type) {
-			var control = Application.Current.FindResource(type);
-			if (control == null) {
+			object control;
+			try {
+				control = Application.Current.FindResource(type);
+			} catch (ResourceReferenceKeyNotFoundException) {
 				return null;
 			}
 
@@ -21,6 +24,16 @@ namespace WpfDefaultTemplateCreator.Models {
 			using var xtw = XmlWriter.Create(sb, xws);
 			XamlWriter.Save(control, xtw);
 			return sb.ToString();
+		}
+
+		public static Type[] GetControlList() {
+			return
+				AppDomain
+					.CurrentDomain
+					.GetAssemblies()
+					.SelectMany(x => x.GetTypes())
+					.Where(x => x.IsSubclassOf(typeof(FrameworkElement)))
+					.ToArray();
 		}
 	}
 }
--- WpfDefaultTemplateCreator/ViewModels/MainWindowViewModel.cs
+++ WpfDefaultTemplateCreator/ViewModels/MainWindowViewModel.cs
@@ -1,13 +1,27 @@
-using System.Windows.Controls;
+using System;
+using System.Reactive.Linq;
+
+using Reactive.Bindings;
 
 using WpfDefaultTemplateCreator.Models;
 
 namespace WpfDefaultTemplateCreator.ViewModels {
 	internal class MainWindowViewModel {
-		public string Xaml {
-			get {
-				return XamlCreator.Create(typeof(GridViewColumnHeader));
-			}
+		public IReadOnlyReactiveProperty<string> Xaml {
+			get;
+		}
+
+		public IReactiveProperty<Type> SelectedControl {
+			get;
+		} = new ReactivePropertySlim<Type>();
+
+		public Type[] Controls {
+			get;
+		}
+
+		public MainWindowViewModel() {
+			this.Controls = XamlCreator.GetControlList();
+			this.Xaml = this.SelectedControl.Select(XamlCreator.Create).ToReadOnlyReactivePropertySlim();
 		}
 	}
 }
